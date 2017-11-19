6358bebd40d1961028e9e61747ae96dd1660b7b3
jean-christophe bauduin <maildejc@hotmail.be>
2017-11-19 16:32:38 +0100

    first commit

---
+++ app/containers/UserPage.js
@@ -0,0 +1,11 @@
+// @flow
+import React, { Component } from 'react';
+import ListUsers from './../components/ListUsers';
+
+export default class UserPage extends Component {
+  render() {
+    return (
+      <ListUsers />
+    );
+  }
+}
